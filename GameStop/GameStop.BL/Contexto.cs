7319991f21d35c57b0f4ac76c1644059623e1f72
AXEL ENRIQUE LINARES VELASQUEZ <axel.linares@unah.hn>
2022-07-13 20:02:10 -0600

    Vídeo_12

---
+++ GameStop/GameStop.BL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStop.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto() : base("GameStopDB")
+        {
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
